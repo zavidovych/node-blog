83f710ac1b9687c5a7970343682de92335f1e1a3
vo.zavidovych@gmail.com <vo.zavidovych@gmail.com>
2012-11-14 23:14:18 -0500

    moved app to github yay

---
+++ app.js
@@ -0,0 +1,46 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(require('stylus').middleware(__dirname + '/public'));
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/users', user.list);
+var articleProvider= new ArticleProvider();
+
+app.get('/', function(req, res){
+  articleProvider.findAll(function(error, docs){
+      res.send(docs);
+  });
+})
+
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});

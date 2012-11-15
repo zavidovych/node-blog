6ad8818bd250e81233cd5e4c6e3de1e80209ea66
Volodymyr Zavidovych <vo.zavidovych@gmail.com>
2012-11-15 01:38:54 -0500

    added form to create posts

--- app.js
+++ app.js
@@ -1,48 +1,61 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express = require('express'),
+    routes = require('./routes'),
+    user = require('./routes/user'),
+    http = require('http'),
+    path = require('path');
 
 var app = express();
 
 var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(path.join(__dirname, 'public')));
+app.configure(function() {
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(require('stylus').middleware(__dirname + '/public'));
+    app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
+app.configure('development', function() {
+    app.use(express.errorHandler());
 });
 
 app.get('/users', user.list);
-var articleProvider= new ArticleProvider();
+var articleProvider = new ArticleProvider();
 
-app.get('/', function(req, res){
-  articleProvider.findAll(function(error, docs){
-    res.render('index.jade', { 
+app.get('/', function(req, res) {
+    articleProvider.findAll(function(error, docs) {
+        res.render('index.jade', {
             title: 'Blog',
-            articles:docs
-            
+            articles: docs
+
         });
-  });
+    });
 })
 
+app.get('/blog/new', function(req, res) {
+    res.render('blog_new.jade', {
+        title: 'New Post'
+    });
+});
+
+app.post('/blog/new', function(req, res) {
+    articleProvider.save({
+        title: req.param('title'),
+        body: req.param('body')
+    }, function(error, docs) {
+        res.redirect('/')
+    });
+});
 
 
 http.createServer(app).listen(app.get('port'), function(){
